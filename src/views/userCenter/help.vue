261b8664ee0ee06bd772340077a3f2fdbede79f8
ghtf <gfdgdgd@re.com>
2018-07-13 20:17:47 +0800

    help

--- src/views/userCenter/help.vue
+++ src/views/userCenter/help.vue
@@ -2,8 +2,11 @@
     <div class="help_page">
         <div class="title">国&nbsp;信&nbsp;诚&nbsp;信&nbsp;宝</div>
         <div class="nr">
-            <div class="tip">您有什么问题或建议想对我们说？</div>
-            <input type="text" placeholder="您的主题：">
+            <div class="theme">您有什么问题或建议想对我们说？</div>
+            <input type="text" v-model="theme" placeholder="您的主题：">
+            <textarea class="textarea" name="" v-model="textarea" id="" rows="12" placeholder="您的宝贵意见，就是我们进步的源泉"></textarea>
+            <p class="tip">请详细描述您遇到的问题，有助于我们快速定位并解决问题，或留下您宝贵的建议或意见，我们会认真评估！</p>
+            <button>问&nbsp;题&nbsp;提&nbsp;交</button>
         </div>
         
 
@@ -18,7 +21,8 @@
         components:{navBars},
         data(){
             return{
-
+                theme:'',
+                textarea:'',
             }
         }
     }
@@ -33,9 +37,9 @@
     bottom: 0;
     background: #F0EFF5;
     .nr{
-        padding: 0.4rem;
+        padding: 0.4rem 0.8rem;
         text-align: left;
-        .tip{
+        .theme{
             font-size: 0.28rem;
             color:#286F99;
             font-weight: 500;
@@ -52,6 +56,31 @@
             border:none;
             box-sizing: border-box;
             margin: 0.2rem 0;
+        }
+        .textarea{
+            width: 99%;
+            border:1px solid rgba(238,238,238,1);
+            background: #fff;
+            text-align: left;
+            font-size: 0.24rem;
+            padding: 0.2rem;
+            box-sizing: border-box;
+            &:focus{
+                outline: none;
+            }
+        }
+        .tip{
+            color: #c0c0c0;
+            font-size: 0.27rem;
+        }
+        button{
+            background: #4070BF;
+            border-radius: 15px;
+            width: 100%;
+            height: 0.9rem;
+            line-height: 0.9rem;
+            color:#fff;
+            font-size: 0.4rem;
         }
     }
     
