b5f05b2fa77e3c0963e8722a1148f85869770958
ghtf <gfdgdgd@re.com>
2018-07-16 13:24:18 +0800

    反馈

--- src/views/feedBack/index.vue
+++ src/views/feedBack/index.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        用户反馈
+    <div class="user_feedback">
+        <div class="title">国&nbsp;信&nbsp;诚&nbsp;信&nbsp;宝</div>
+        <div class="nr">
+            <ul class="clearfix feedback_list">
+                <li v-for="(item,index) in dataList" :key="index">
+                    <img :src="item.imgUrl" alt="">
+                </li>  
+            </ul>
+        </div>
         <nav-bars></nav-bars>
     </div>
 </template>
@@ -12,8 +19,79 @@
         components:{navBars},
         data(){
             return{
-
+                dataList:[
+                    {
+                        imgUrl: require('../../assets/imgs/img1.png'),
+                        id: 1
+                    },
+                    {
+                        imgUrl: require('../../assets/imgs/img1.png'),
+                        id: 1
+                    },
+                    {
+                        imgUrl: require('../../assets/imgs/img1.png'),
+                        id: 1
+                    },
+                    {
+                        imgUrl: require('../../assets/imgs/img1.png'),
+                        id: 1
+                    },
+                    {
+                        imgUrl: require('../../assets/imgs/img1.png'),
+                        id: 1
+                    },
+                    {
+                        imgUrl: require('../../assets/imgs/img1.png'),
+                        id: 1
+                    },
+                    {
+                        imgUrl: require('../../assets/imgs/img1.png'),
+                        id: 1
+                    },
+                    {
+                        imgUrl: require('../../assets/imgs/img1.png'),
+                        id: 1
+                    }
+                ]
             }
         }
     }
 </script>
+
+<style lang="scss">
+.user_feedback{
+    margin-bottom: 1.5rem;
+    .nr{
+        padding: 0.15rem;
+        box-sizing: border-box;
+    }
+    ul.feedback_list{
+        li{
+            float: left;
+            width: 32%;
+            margin: 0 0.08rem 0.1rem 0;
+            img{
+                width: 100%;
+                height: 3.4rem;
+            }
+        }
+        li:nth-child(1){
+            width: 49%;
+            margin: 0 0.1rem 0 0;
+            img{
+                height: 5.3rem;
+            }
+        }
+        li:nth-child(2){
+            width: 49%;
+            margin-right: 0;
+            img{
+                height: 5.3rem;
+            }
+            
+        }
+        
+    }
+}
+
+</style>
